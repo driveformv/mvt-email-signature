0cb984539857fdecda0f28702258a35b4f6e811b
noelgarces <noel.garces@m-v-t.com>
2021-12-21 11:26:25 -0700

    Use 40 year mvt logo

--- src/components/Signature/Logo.js
+++ src/components/Signature/Logo.js
@@ -6,7 +6,8 @@ const Logo = () => {
 			{/* <img border="0" src={MVTLogo} alt="Logo" width={80} /> */}
 			<img
 				border="0"
-				src="https://driveformvt.com/wp-content/uploads/2021/06/MVT-Globe-email.png"
+				// src="https://driveformvt.com/wp-content/uploads/2021/06/MVT-Globe-email.png"
+				src="https://m-v-t.com/wp-content/uploads/2021/12/mvt-logo-40-year-anniversary.png"
 				alt="Logo"
 				width={80}
 			/>

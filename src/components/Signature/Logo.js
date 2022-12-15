3ffdb4eba13fc09863809ff45c950bc849cb3858
Noel Garces <ganoe@m-v-t.com>
2022-12-15 10:57:01 -0700

    Update

--- src/components/Signature/Logo.js
+++ src/components/Signature/Logo.js
@@ -1,18 +1,18 @@
 // import MVTLogo from "../../assets/images/mvt-logo.png";
 
 const Logo = () => {
-	return (
-		<a href="https://www.m-v-t.com/" target="_blank" rel="noreferrer">
-			{/* <img border="0" src={MVTLogo} alt="Logo" width={80} /> */}
-			<img
-				border="0"
-				// src="https://driveformvt.com/wp-content/uploads/2021/06/MVT-Globe-email.png"
-				src="https://imagedelivery.net/VcyCX-G1STM0PJ3dh1L69A/5af68d4f-d7af-4f3a-60f1-09a00101ab00/public"
-				alt="Logo"
-				width={80}
-			/>
-		</a>
-	);
+  return (
+    <a href="https://www.m-v-t.com/" target="_blank" rel="noreferrer">
+      {/* <img border="0" src={MVTLogo} alt="Logo" width={80} /> */}
+      <img
+        border="0"
+        src="https://driveformvt.com/wp-content/uploads/2021/06/MVT-Globe-email.png"
+        // src="https://imagedelivery.net/VcyCX-G1STM0PJ3dh1L69A/5af68d4f-d7af-4f3a-60f1-09a00101ab00/public"
+        alt="Logo"
+        width={80}
+      />
+    </a>
+  );
 };
 
 export default Logo;

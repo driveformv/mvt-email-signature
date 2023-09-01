32894243b2228e53a31e03570a3fd4ab97378b84
ganoe <ganoe@m-v-t.com>
2023-08-31 21:53:48 -0600

    Replace signature assets with azure storage account links

--- src/contexts/companies.tsx
+++ src/contexts/companies.tsx
@@ -3,52 +3,52 @@ import { useContext, createContext, useState, useMemo, useCallback, ReactElement
 const companies = {
   MESILLA_VALLEY_TRANSPORTATION: {
     name: 'Mesilla Valley Transportation',
-    logoURL: 'https://m-v-t.com/wp-content/uploads/2023/08/logo.png',
+    logoURL: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/mvt/logo.png',
     websiteURL: 'www.m-v-t.com',
     brandColor: '#c02125',
     socialMedia: {
       facebook: {
-        icon: 'https://i.ibb.co/dbYP654/FB-email-red-01.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/mvt/facebook-icon.png',
         source: 'https://www.facebook.com/mesillavalleytransport/',
       },
       twitter: {
-        icon: 'https://i.ibb.co/ZKWhkby/Twitter-email-red-01.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/mvt/twitter-icon.png',
         source: 'https://twitter.com/MVTServices',
       },
       youtube: {
-        icon: 'https://i.ibb.co/MsFBYJ7/YT-email-red-01.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/mvt/youtube-icon.png',
         source: 'https://www.youtube.com/channel/UCANz_NAedSbFwiBrkZ8c5nA',
       },
       linkedin: {
-        icon: 'https://i.ibb.co/hYKScqb/linkedin-email-red-01.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/mvt/linkedin-icon.png',
         source: 'https://www.linkedin.com/company/m-v-t/',
       },
       instagram: {
-        icon: 'https://i.ibb.co/GtX9K3Z/IG-email-red-01.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/mvt/instagram-icon.png',
         source: 'https://www.instagram.com/mesillavalleytransportation/',
       },
     },
   },
   BORDER_TIRE: {
     name: 'Border Tire',
-    logoURL: '/assets/logos/border-tire.png',
+    logoURL: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/border-tire/logo.png',
     websiteURL: 'www.border-tire.com',
     brandColor: '#D14905',
     socialMedia: {
       facebook: {
-        icon: '/assets/signature/social-media-icons/border-tire-facebook.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/border-tire/facebook-icon.png',
         source: 'https://www.facebook.com/BorderTire',
       },
       youtube: {
-        icon: '/assets/signature/social-media-icons/border-tire-youtube.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/border-tire/youtube-icon.png',
         source: 'https://www.youtube.com/@bordertire6249/featured',
       },
       linkedin: {
-        icon: '/assets/signature/social-media-icons/border-tire-linkedin.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/border-tire/linkedin-icon.png',
         source: 'https://www.linkedin.com/company/border-tire/',
       },
       instagram: {
-        icon: '/assets/signature/social-media-icons/border-tire-instagram.png',
+        icon: 'https://saprodmrktngemailsigw2.blob.core.windows.net/assets/border-tire/instagram-icon.png',
         source: 'https://www.instagram.com/bordertire/',
       },
     },

007c8837c8a054d05fa7684e1b3ed6a2d1f108ee
ganoe <noel.garces@m-v-t.com>
2023-10-09 22:16:59 -0600

    Dynamic logo dimensions

--- src/components/Signature/Logo.js
+++ src/components/Signature/Logo.js
@@ -1,11 +1,22 @@
-import { useCompanies } from 'contexts/companies';
+import { useCompanies } from "contexts/companies";
 
 const Logo = () => {
   const company = useCompanies();
 
+  const logoImgDimension = {
+    STAGECOACH: {
+      width: 120,
+    },
+  }[company.id] ?? { width: 80 };
+
   return (
     <a href={`https://${company.websiteURL}`} target="_blank" rel="noreferrer">
-      <img border="0" src={company.logoURL} alt="Logo" width={80} />
+      <img
+        border="0"
+        src={company.logoURL}
+        alt="Logo"
+        width={logoImgDimension.width}
+      />
     </a>
   );
 };
